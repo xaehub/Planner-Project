5370c121d0342d5a92f1a03e1fb5e4f40a492edd
xaehub <novxae@gmail.com>
2025-04-02 15:26:52 +0900

    feat: JPA 설정 추가 및 README 작성

---
+++ src/main/java/com/example/planner/entity/BaseEntity.java
@@ -0,0 +1,12 @@
+package com.example.planner.entity;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+}
